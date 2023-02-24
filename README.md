7e2dcdf12c48c735033ee9e266cad22481d5e2e2
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-02-24 18:42:33 +0900

    update: readme

--- README.md
+++ README.md
@@ -4,3 +4,25 @@
 - Blog by tutorial and some feature.
 - simpleに
 
+
+## Adding new tags
+
+For adding new tags to posts you need to add the new tag in the array to the post located in `src/pages/blog`. For example `tags: ["general", "tech", "nature", "new-tag"]`.
+
+After adding this new tag you need to add this tag into the `[tag].astro` file that the dynamic route is created and when building the site. Here you need to add the new tag as a param in the `getStaticPaths` method.
+
+```javascript
+export function getStaticPaths() {
+  return [
+    { params: { tag: "general" } },
+    { params: { tag: "tech" } },
+    { params: { tag: "food" } },
+    { params: { tag: "nature" } },
+    { params: { tag: "astro" } },
+    // added this new-tag
+    { params: { tag: "new-tag" } },
+  ];
+}
+```
+
+The tags will be read out of all posts dynamically using the `findTags` method in `src/utils/post.utils`.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,53 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  site: "https://sunny-arithmetic-1e866e.netlify.app",
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    compress({
+      css: true,
+      html: false,
+      js: true,
+      img: false,
+      svg: false,
+    }),
+    robotsTxt({
+      sitemapBaseFileName: "sitemap-index",
+      policy: [
+        {
+          userAgent: "Googlebot",
+          allow: "/",
+          crawlDelay: 2,
+        },
+      ],
+    }),
+
+    webmanifest({
+      name: "memo代わりのチラシの裏",
+      icon: "./public/favicon.svg",
+      lang: "ja-JP",
+      short_name: "チラウラ",
+      description: "日々の徒然、メモ等を置いておく。",
+      theme_color: "#4E9F3D",
+      background_color: "#4E9F3D",
+      display: "standalone",
+    }),
+  ],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
